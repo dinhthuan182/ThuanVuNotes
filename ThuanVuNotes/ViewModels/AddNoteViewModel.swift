2c908a0472b41cc557e7ff302ec4285aae7fffcf
Thuận Vũ <>
2023-11-29 21:02:46 +0700

    feat: create UI for add note screen

---
+++ ThuanVuNotes/ViewModels/AddNoteViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  AddNoteViewModel.swift
+//  ThuanVuNotes
+//
+//  Created by Thuận Vũ on 29/11/2023.
+//
+
+import Foundation
+
+// MARK: AddNoteViewModel
+class AddNoteViewModel: ObservableObject {
+    // MARK: Properties
+    @Published var content = ""
+}
--- ThuanVuNotes/Views/AddNote.swift
+++ ThuanVuNotes/Views/AddNote.swift
@@ -7,8 +7,33 @@
 
 import SwiftUI
 
+// MARK: AddNote
 struct AddNote: View {
+    // MARK: Properties
+    @Environment(\.dismiss) var dismiss
+    @ObservedObject var viewModel = AddNoteViewModel()
+    @FocusState private var editingNote: Bool
+
     var body: some View {
-        Text("Add note")
+        ZStack(alignment: .bottomTrailing) {
+            TextEditor(text: $viewModel.content)
+                            .foregroundStyle(.secondary)
+                            .padding(.horizontal)
+                            .focused($editingNote)
+
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "plus.circle")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .padding([.bottom, .trailing], 10)
+            }
+        }
+        .navigationTitle("New note")
+        .navigationBarTitleDisplayMode(.inline)
+        .onAppear {
+            editingNote.toggle()
+        }
     }
 }
