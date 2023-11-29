a7aeaa44d83fccb2353a830b6b92313ffc7abf07
Thuận Vũ <>
2023-11-29 14:54:01 +0700

    feat: create project

---
+++ ThuanVuNotes/ThuanVuNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThuanVuNotesApp.swift
+//  ThuanVuNotes
+//
+//  Created by Thuận Vũ on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThuanVuNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
