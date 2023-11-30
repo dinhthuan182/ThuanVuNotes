4601b130b6f1f9d32107cc298f78bf255180fed3
Thuận Vũ <>
2023-11-30 16:12:45 +0700

    feat: enable offline database

--- ThuanVuNotes/App/AppDelegate.swift
+++ ThuanVuNotes/App/AppDelegate.swift
@@ -11,8 +11,10 @@ import Firebase
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Config Firebase
         FirebaseApp.configure()
         Auth.auth().signInAnonymously()
+        Database.database().isPersistenceEnabled = true
 
         return true
     }
